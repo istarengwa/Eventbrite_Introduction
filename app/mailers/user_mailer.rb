223e46b937ede6915b69e0bb6d35631e604ffd90
Istarengwa <muller.lilian@hotmail.fr>
2021-11-08 19:02:44 +0100

    add mails for event

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,16 @@ class UserMailer < ApplicationMailer
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
+
+  def new_guest_send(attendance)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @attendance = attendance
+    @admin = User.find(@attendance.event.admin_id)
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @admin.email, subject: 'Vous avez un nouvel invité !') 
+  end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,4 +3,9 @@ class Attendance < ApplicationRecord
   belongs_to :event
   validates :user, presence: true
   validates :event, presence: true
+  after_create :new_guest_send
+
+  def new_guest_send
+    UserMailer.new_guest_send(self).deliver_now
+  end
 end
