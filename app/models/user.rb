1b319e24958ddabb7787750ababb59909e3c807a
Istarengwa <muller.lilian@hotmail.fr>
2021-11-08 17:29:28 +0100

    mailer test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+  after_create :welcome_send
 
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
