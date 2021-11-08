42fcd63866c31bb02f34d7416149ab9a51a532a0
Istarengwa <muller.lilian@hotmail.fr>
2021-11-08 16:31:33 +0100

    add controler

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :events, foreign_key: 'admin_id', class_name: "Event"
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
