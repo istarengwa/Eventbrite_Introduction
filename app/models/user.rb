d7c8e1049b5144c6029912cb21fcc1eae99a83dc
Istarengwa <muller.lilian@outlook.fr>
2023-03-01 14:20:28 +0100

    add new info

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :events, foreign_key: 'admin_id', class_name: "Event"
   #after_create :welcome_send
+  has_one_attached :avatar
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
